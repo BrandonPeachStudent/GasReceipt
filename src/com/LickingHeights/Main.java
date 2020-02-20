1b8fec9a86c9b461612e0353fab639dc5757d883
Brandon Peach <brandon.peach.2022@student.lhschools.org>
2020-02-20 10:58:18 -0500

    intellij commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,34 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("+-----------------------------+");
+        System.out.println("|Peach Gas Station            |");
+        System.out.println("|1234 Abc Dr                  |");
+        System.out.println("|1235 Bcd Rd                  |");
+        System.out.print("|Date");
+        System.out.println("               Time      |");
+        System.out.print("|13JAN2020");
+        System.out.println("|         12:30 PM  |");
+        System.out.println("|Invoice Number#    123456    |");
+        System.out.println("|Auth#              001122    |");
+        System.out.println("|Mastercard                   |");
+        System.out.println("|Account Number               |");
+        System.out.println("|123456789101                 |");
+        System.out.print("|pump");
+        System.out.print("      Product");
+        System.out.println("       $/G  |");
+        System.out.print("| 01");
+        System.out.print("       unleaded");
+        System.out.println("     $2.79 |");
+        System.out.print("|Amount");
+        System.out.println("      Fuel Total       |");
+        System.out.print("|20.00");
+        System.out.println("        $55.80          |");
+        System.out.println("|    Thank you for using      |");
+        System.out.println("|         Peach Gas           |");
+        System.out.println("+-----------------------------+");
+
+    }
+}
